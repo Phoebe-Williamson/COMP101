4ee0775c5e2c2b3442b1b357c1ba37c40d8fcad6
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2024-02-29 10:34:19 +1300

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,51 @@
+##
+# A file in the first lab
+# date (29/02/24)
+#
+'''
+# print Hello World
+print("Hello World")
+
+#using the type function
+print(type(4))  # type int
+print(type(7.9))  # type float
+print(type("Hello"))  #type string
+print(type("50.0"))  #type string
+
+# testing and  fixng name error (added value for age)
+age = 12
+print(age)
+
+# testing and fixing type error (changed '+' to ',')
+age = 12
+name = "jess"
+print(age, name)
+
+# using 3 print statements
+print("A wizard is never late, Frodo Baggins.")
+print("Nor is he early.")
+print("He arrives precisely when he means to!")
+
+# classmates name and hobbie test
+name = "Isabella"
+hobby = "Ultimate frisbe"
+print("My classmate is ", name)
+print("Their favourite hobby is ", hobby)
+
+# print out net_income
+tax_rate = 0.30
+gross_income = 5000
+
+#add your calculation here
+tax_rate = tax_rate*gross_income
+net_income = gross_income-tax_rate
+print(net_income)
+'''
+# making a sentnce
+# variables given
+first_name = "Meera"
+last_name = "Patel"
+age = "27"
+
+#print statement
+print(first_name, last_name, "is", age, "years old.")
