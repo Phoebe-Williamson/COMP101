956b4a5351ee49f9b761209091cb27220b113f52
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2024-03-25 10:49:45 +1300

    done stuff for q6 and write descripitons for questions still to do

--- Lab_7.py
+++ Lab_7.py
@@ -40,8 +40,8 @@ elif "123" in password or "abc" in password:
     print("Your password is invalid!")
 else:
     print("Your password is valid!")
-'''
-#
+
+#Q5
 movie_list = []
 keep_going = True
 while keep_going:
@@ -55,10 +55,42 @@ print()
 print("Movies List:")
 print("==============")
 print(movie_list)
+'''
+#q6
+current_upis = ['cron777', 'emac263', 'gclo450',
+                'jcoc100', 'jduj117', 'lmes754', 'mjac999']
+current_passwords = ['SuiiiAlNassr23', 'PcqcNP2022', 'WhatElse06',
+                     'HatOn4414', 'BlanQueTTe2006', 'GoatWC2022', 'thriLLer82']
 
+print("Current list of UPIs:", current_upis)
+print("Current list of passwords:", current_passwords)
 
+keep_running = True
+while keep_running:
+    letter = input("Enter A to add a UPI/password pair or D to delete a pair: ")
+    if letter.upper() == "A": #add upi
+        new_upi = input("Enter a new UPI: ")
+        if new_upi in current_upis:
+            print(f"{new_upi} is already used!")
+            new_upi = input("Enter a new UPI: ")
+        else:
+            current_upis.append(new_upi)
+            new_password = input("Enter a new password: ")
+            current_passwords.append(new_password)
+    elif letter.upper() == "D": #delete upi
+        upi_to_delete = input("Enter the UPI to be deleted: ")
+        if upi_to_delete not in current_upis:
+            print("UPI does not match!")
+            upi_to_delete = input("Enter the UPI to be deleted: ")
+        else:
+            #find upis key, find that key in passwords, set that to password
+            #check if the password_entered is equal to password,
+            #if not, say it does not match and ask again
+            # if password matches, remove upi an dpassowrd from the lists
 
-
+#Q7 need to add to list, reverse lost and then print out with comma between 
+#and make sure there a no repetitions
+sentence = input("Please enter a sentence: ")
 
 
 
